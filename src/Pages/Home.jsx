9ca15bed174c06c6d97e90fa35f2db159ecc16c7
alpha <godspowerlawrence008@gmail.com>
2024-09-23 06:33:48 -0400

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -269,7 +269,7 @@ export default function Home(){
                         ></span>
                         <span
                             class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white"
-                            >LEARN MORE</span>
+                            >Learn More</span>
                         </Link>
                            </div>
 
