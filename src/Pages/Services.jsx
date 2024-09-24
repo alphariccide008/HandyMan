abe5b549059fc8de91394cde820ec91840e94051
alpha <godspowerlawrence008@gmail.com>
2024-09-24 11:06:06 -0400

    update

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -32,7 +32,7 @@ const services = () => {
                     <div className="text-center">
                         <h1 className='text-4xl  font-bold md:mx-16 mb-5 text-center 
                         '>BATHROOM REMODELLING</h1>
-                        <p className='lg:px-10 text-center py-5' style={{fontFamily: 'Poppins, sans-serif'}}>All-Handyman   offers expert bathroom remodeling solutions designed to elevate both the functionality and aesthetics of your space. Whether you're dreaming of a spa-like retreat, upgrading outdated fixtures, or maximizing storage, our skilled professionals can bring your ideas to life. We focus on delivering quality craftsmanship while providing fair pricing and flexible financing options to fit any budget. Trust Handyman to turn your bathroom into a space you’ll love for years to come!
+                        <p className='lg:px-10 lg:text-start py-5' style={{fontFamily: 'Poppins, sans-serif'}}>All-Handyman   offers expert bathroom remodeling solutions designed to elevate both the functionality and aesthetics of your space. Whether you're dreaming of a spa-like retreat, upgrading outdated fixtures, or maximizing storage, our skilled professionals can bring your ideas to life. We focus on delivering quality craftsmanship while providing fair pricing and flexible financing options to fit any budget. Trust Handyman to turn your bathroom into a space you’ll love for years to come!
 </p>
                         <p className='text-center mb-5 md:hidden'>______________________________</p>
                         
@@ -70,7 +70,7 @@ const services = () => {
                     <div className="text-center">
                         <h1 className='text-4xl  font-bold md:mx-16 mb-5 text-center 
                         '>KITCHEN REMODELLING</h1>
-                        <p className='lg:px-10 text-center py-5' style={{fontFamily: 'Poppins, sans-serif'}}>
+                        <p className='lg:px-10 lg:text-start py-5' style={{fontFamily: 'Poppins, sans-serif'}}>
                             At All-Handyman, we specialize in transforming kitchens into beautiful, efficient, and functional spaces that suit your lifestyle. Whether you're looking to modernize your kitchen with new cabinets, countertops, or appliances, or you need a full redesign, our experienced team delivers exceptional craftsmanship. With our commitment to fair pricing and flexible financing, your dream kitchen is within reach. Let Handyman create a kitchen that’s perfect for cooking, entertaining, and enjoying every day!
                         </p>
                         <p className='text-center mb-5 md:hidden'>______________________________</p>
@@ -112,7 +112,7 @@ const services = () => {
                     <div className="text-center">
                         <h1 className='text-4xl  font-bold md:mx-16 mb-5 text-center 
                         '>BEDROOM REMODELLING</h1>
-                        <p className='lg:px-10 text-center py-5' style={{fontFamily: 'Poppins, sans-serif'}}>
+                        <p className='lg:px-10 lg:text-start py-5' style={{fontFamily: 'Poppins, sans-serif'}}>
                             At All-Handyman, we specialize in creating beautiful, functional bedroom spaces tailored to your personal style and needs. Whether you're looking to modernize your bedroom with updated fixtures, add storage, or create a cozy retreat, our skilled team offers a range of services to bring your vision to life. With fair pricing, flexible financing options, and attention to detail, we ensure your bedroom remodel is stress-free and meets your expectations. Let Handyman help you create the bedroom of your dreams!
                         </p>
                         <p className='text-center mb-5 md:hidden'>______________________________</p>
@@ -151,7 +151,7 @@ const services = () => {
                     <div className="text-center">
                         <h1 className='text-4xl  font-bold md:mx-16 mb-5 text-center 
                         '>SITTING-ROOM REMODELLING</h1>
-                        <p className='lg:px-10 text-center py-5' style={{fontFamily: 'Poppins, sans-serif'}}>
+                        <p className='lg:px-12 lg:text-start py-5' style={{fontFamily: 'Poppins, sans-serif'}}>
                             All-Handyman is here to help you transform your sitting room into a stylish, comfortable space that fits your lifestyle. Whether you’re looking to create a modern, cozy environment or maximize space with clever design solutions, our skilled team is ready to bring your vision to life. With our focus on quality, fair pricing, and flexible financing options, we ensure your sitting room remodel is seamless and stress-free. Trust Handyman to make your
                         </p>
                         <p className='text-center mb-5 md:hidden'>______________________________</p>
